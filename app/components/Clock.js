c600005fab8815ff839b334617a70ff38fd07895
quangthuyhoang <qtnhoang@gmail.com>
2018-12-22 23:43:58 -0600

    Finalize CLock Animation for ClockComponent
    Added inline styles that transitions wtih interval state changes

--- app/components/Clock.js
+++ app/components/Clock.js
@@ -9,7 +9,82 @@ type Props = {};
 export default class Clock extends Component<Props> {
   props: Props;
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      seconds: new Date().getSeconds().toLocaleString(),
+      minute: new Date().getMinutes().toLocaleString(),
+      hour: new Date().getHours().toLocaleString(),
+    };
+  }
+
+  componentDidMount() {
+    this.intervalID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
+
+  tick = () => {    
+    const hr = new Date().getHours().toLocaleString();
+    this.setState({
+      seconds: new Date().getSeconds().toLocaleString(),
+      minute: new Date().getMinutes().toLocaleString(),
+      hour: ( hr > 12) ? hr - 12 : hr,
+    });
+  }
+
+  getSecondsDegree = (seconds) => {
+    return ((seconds / 60) * 360) + 90;
+  }
+
+  getHourDegree = (hr) => {
+    return ((hr / 12) * 360) + 90;
+  }
+
   render() {
+    const { seconds, minute, hour } = this.state;
+    const clockStyles = {
+      secHand: {
+        width: `50%`,
+        height: `3px`,
+        background: `black`,
+        position: `absolute`,
+        top: `50%`,
+        transformOrigin: `100%`,
+        transform:` rotate(${this.getSecondsDegree(seconds)}deg)`,
+        transition: `all 0.05s`,
+        'transition-timing-function': `cubic-bezier(0.26, 3.46, 0.58, 1)`,
+      },
+      minHand: {
+        width: `50%`,
+        height: `5px`,
+        background: `black`,
+        position: `absolute`,
+        top: `50%`,
+        transformOrigin: `100%`,
+        transform:` rotate(${this.getSecondsDegree(minute)}deg)`,
+        transition: `all 0.05s`,
+        'transition-timing-function': `cubic-bezier(0.26, 3.46, 0.58, 1)`,
+      },
+      hrHand: {
+        width: `50%`,
+        height: `7px`,
+        background: `black`,
+        position: `absolute`,
+        top: `50%`,
+        transformOrigin: `100%`,
+        transform:` rotate(${this.getHourDegree(hour)}deg)`,
+        transition: `all 0.05s`,
+        'transition-timing-function': `cubic-bezier(0.26, 3.46, 0.58, 1)`,
+      }
+    }
+
+    
     return (
       <div>
         <div className={styles.backButton} data-tid="backButton">
@@ -18,8 +93,12 @@ export default class Clock extends Component<Props> {
           </Link>
         </div>
         <h2>Clock</h2>
-        <div className="clock">
-          Hello
+        <span>Time is {hour}:{minute}:{seconds} </span>
+        <div className={styles.clock}>
+          <div className={styles.clockFace}/>
+          <div style={clockStyles.hrHand} />
+          <div style={clockStyles.minHand} />
+          <div style={clockStyles.secHand} />
         </div>
       </div>
     );
